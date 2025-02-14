a05fd0c92b716c733826b627fd84fe78d527087b
Thomas-pi-gi <thomas.pierlot01@gmail.com>
2025-02-14 16:04:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const cardPool = [
-    'BG', 'Marco', 'RV', 'Pauline', 'TG', 'T.Kratz'
+    'BG', 'Marco', 'RV', 'Pauline', 'TG', 'T.Kratz', 'person7', 'person8'
 ];
 
 let remainingCards = [...cardPool];
@@ -11,34 +11,62 @@ const option2 = document.getElementById('option2');
 const result = document.getElementById('result');
 const overlay1 = document.getElementById('overlay1');
 const overlay2 = document.getElementById('overlay2');
+const roundTitle = document.getElementById('roundTitle');
+const winnerSection = document.getElementById('winnerSection');
+const winnerImg = document.getElementById('winnerImg');
+const winnerName = document.getElementById('winnerName');
 
 function getRandomCard() {
     const randomIndex = Math.floor(Math.random() * remainingCards.length);
     return remainingCards.splice(randomIndex, 1)[0];
 }
 
+function updateRoundTitle() {
+    const rounds = {
+        16: "Round of 16",
+        8: "Quarter Final",
+        4: "Semi Final",
+        2: "Final"
+    };
+    roundTitle.innerText = rounds[remainingCards.length] || "Final";
+}
+
 function displayCards() {
+    if (remainingCards.length === 1) {
+        showWinner(remainingCards[0]);
+        return;
+    }
+
+    updateRoundTitle();
+
     const card1 = getRandomCard();
     const card2 = getRandomCard();
 
     card1Img.src = `images/${card1}.jpg`;
     card2Img.src = `images/${card2}.jpg`;
 
-    overlay1.style.display = 'none';
-    overlay2.style.display = 'none';
+    overlay1.classList.remove('show');
+    overlay2.classList.remove('show');
 
     option1.onclick = () => chooseCard(card1, card2);
     option2.onclick = () => chooseCard(card2, card1);
 }
 
 function chooseCard(chosenCard, eliminatedCard) {
-    result.innerText = `${chosenCard} avance à la prochaine chouffe!`;
-    if (eliminatedCard === card1Img.src.split('/').pop().replace('.jpg', '')) {
-        overlay1.style.display = 'flex';
-    } else {
-        overlay2.style.display = 'flex';
-    }
-    setTimeout(displayCards, 2000); // Display new cards after 2 seconds
-}
+    result.innerText = `${chosenCard} moves to the next round!`;
 
-displayCards();
+    if (eliminatedCard === card1Img.src.split('/').pop().replace('.jpg', '')) {
+        overlay1.classList.add('show');
+    } else {
+        overlay2.classList.add('show');
+    }
+
+    option1.disabled = true;
+    option2.disabled = true;
+
+    setTimeout(() => {
+        option1.disabled = false;
+        option2.disabled = false;
+        remainingCards.push(chosenCard);
+        displayCards();
+    }, 2000
